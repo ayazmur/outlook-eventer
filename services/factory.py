b7a58f461a88970c15105ba5e23a54d64fc6aab9
ayaz <ayazikmur@gmail.com>
2025-05-30 00:14:50 +0500

    feat: наработки для настоящего аутлука

--- services/factory.py
+++ services/factory.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 from interfaces.ioutlook import ICalendarService
 from interfaces.iconnection import IUserInterface
 from services.outlook_mock_service.outlook_mock import MockOutlookService
+from services.outlook_service.outlook_service import OutlookCalendarService
 from bots.telegram_bot.bot import TelegramBot
 
 load_dotenv()
-
 
 class ServiceFactory:
     @staticmethod
@@ -15,6 +15,10 @@ class ServiceFactory:
 
         if service_type == 'mock':
             return MockOutlookService()
+        elif service_type == 'outlook':
+            return OutlookCalendarService()
+        else:
+            raise ValueError(f"Unknown calendar service type: {service_type}")
 
     @staticmethod
     async def create_user_interface(calendar_service: ICalendarService) -> IUserInterface:
